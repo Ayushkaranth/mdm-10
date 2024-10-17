b0bcba183d4c99acf793063991338aedc92454fa
ayush karanth <160300874+Ayushkaranth@users.noreply.github.com>
2024-10-17 19:13:01 +0530

    Create WebsiteTest.java

---
+++ src/test/java/WebsiteTest.java
@@ -0,0 +1,12 @@
+package src.test.java;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class WebsiteTest {
+
+    @Test
+    public void testWelcomeMessage() {
+        String message = "Welcome to Jenkins";
+        assertEquals("Welcome to Jenkins", message);
+    }
+}
